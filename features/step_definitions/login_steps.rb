507c71425a42a7f7871c50b04d51ea4d1fedf45a
Matheus Evangelista <evangelista.developer@gmail.com>
2019-01-17 10:06:34 -0200

    add login steps

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,11 @@
+Dado("que eu esteja na página principal do sistema") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Quando("faço o login com o com o email {string} e senha {string}") do |email, senha|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Entao("devo ser autenticado com sucesso") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
