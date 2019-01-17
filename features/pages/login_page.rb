07a7c76a613252ffd2954f9ffcd3a6d560f33c3b
Matheus Evangelista <evangelista.developer@gmail.com>
2019-01-17 12:10:30 -0200

    add selector and update steps

--- features/pages/login_page.rb
+++ features/pages/login_page.rb
@@ -1,5 +1,23 @@
 # encoding: utf-8
 
 class LoginPage < SitePrism::Page
-    
+
+  # Declare todos os elementos da página
+  # element :button,       pending 'Inserir o seletor do botão'
+  element :btn_submit, 'input[name="commit"]'
+  element :input_email, '#login_field'
+  element :input_senha, '#password'
+
+
+  def preenche_formulario(email, senha)
+
+    input_email.set email
+    input_senha.set senha
+
+  end
+
+  def clicar_submit
+    btn_submit.click
+  end
+
 end
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -4,7 +4,12 @@ Dado("que eu esteja na página principal do sistema") do
 end
   
 Quando("faço o login com o com o email {string} e senha {string}") do |email, senha|
-    pending # Write code here that turns the phrase above into concrete actions
+
+    @login.preenche_formulario(email, senha)
+    sleep 2
+    @login.clicar_submit
+    sleep 5
+    
 end
   
 Entao("devo ser autenticado com sucesso") do
